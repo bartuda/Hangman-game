2d6e8f0c0afd5bf32a6f6f6a73d3071ade176954
Dan <bartuda@gmail.com>
2021-02-02 17:57:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+#Source https://www.youtube.com/watch?v=5x6iAKdJB6U&ab_channel=NeuralNine
+
+import random
+
+with open('Wordlist.txt', 'r') as f:
+    words = f.readlines()
+
+word = random.choice(words)[:-1]
+
+allowed_errors = 7
+guesses = []
+done = False
+
+while not done:
+    for letter in word:
+        if letter.lower() in guesses:
+            print(letter, end=" ")
+        else:
+            print("_", end=" ")
+    print("")
+
+    guess = input(f"Allowed Errors Left {allowed_errors}, Next Guess: ")
+    guesses.append(guess.lower())
+    if guess.lower() not in word.lower():
+        allowed_errors -= 1
+        if allowed_errors == 0:
+            break
+
+    done = True
+    for letter in word:
+        if letter.lower() not in guesses:
+            done = False
+
+if done:
+    print(f"You found the word! It was {word}!")
+else:
+    print(f"Game Over! The word was {word}!")
